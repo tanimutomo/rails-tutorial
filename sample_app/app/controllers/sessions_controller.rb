752dbeb629dd5999eb94f0b1f30fefedaba9398d
tanimutomo <tanimutomo@gmail.com>
2020-08-20 10:08:48 +0900

    add: session create with login

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # redirect user info page
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'invalid email/password combination'
       render 'new'
