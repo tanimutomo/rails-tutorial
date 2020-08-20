18519031cb96136c7f41d3e59a641dc5bab1a67f
tanimutomo <tanimutomo@gmail.com>
2020-08-20 09:53:04 +0900

    add: user login form

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # redirect user info page
+    else
+      flash.now[:danger] = 'invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
