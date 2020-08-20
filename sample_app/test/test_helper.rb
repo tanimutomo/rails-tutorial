10ea40e172f595f9acbbc853f8e0c9c5950cdfa2
tanimutomo <tanimutomo@gmail.com>
2020-08-20 10:44:32 +0900

    add: logout

--- sample_app/test/test_helper.rb
+++ sample_app/test/test_helper.rb
@@ -1,10 +1,13 @@
+# frozen_string_literal: true
+
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
