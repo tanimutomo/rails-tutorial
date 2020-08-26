47e7d5e4a9f0609c3150cb4f255126a3557ec15f
tanimutomo <tanimutomo@gmail.com>
2020-08-26 11:24:05 +0900

    add: logged in user site layout test

--- sample_app/test/integration/site_layout_test.rb
+++ sample_app/test/integration/site_layout_test.rb
@@ -1,11 +1,34 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test 'layout links' do
+  def setup
+    @user = users(:michael)
+  end
+
+  test 'non login user layout links' do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
     assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', login_path
+
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+  end
+
+  test 'login user layout links' do
+    log_in_as(@user)
+    get root_path
+
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', users_path
+
+    assert_select 'a[href=?]', user_path(@user)
+    assert_select 'a[href=?]', edit_user_path(@user)
+    assert_select 'a[href=?]', logout_path
+
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', contact_path
   end
