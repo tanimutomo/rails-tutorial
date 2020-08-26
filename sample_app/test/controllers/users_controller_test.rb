2b936e93d50cfbf2d136e2f4d1df6c81016216ec
tanimutomo <tanimutomo@gmail.com>
2020-08-26 11:23:50 +0900

    add: users index page

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -1,9 +1,18 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
+  test 'should redirect index when not logged in' do
+    get users_path
+    assert_redirected_to login_url
+  end
+
+  test 'should get new' do
     get signup_path
     assert_response :success
   end
-
 end
