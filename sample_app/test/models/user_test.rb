55ea37b1976192df7848b5a692dca0edbe91bf99
tanimutomo <tanimutomo@gmail.com>
2020-08-27 12:26:45 +0900

    add: acount activation and email

--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -68,6 +68,6 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'authenticated? shoudl return false for a user with nil digest' do 
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
