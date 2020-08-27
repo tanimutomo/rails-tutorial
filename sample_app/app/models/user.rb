02d895c4e980e35f8ff259ce47384cf6eedf3eae
tanimutomo <tanimutomo@gmail.com>
2020-08-27 11:03:21 +0900

    add: activations logic

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  attr_accessor :remember_token
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
-  before_save { self.email = email.downcase }
+  attr_accessor :remember_token, :activation_token
+
+  before_save :downcase_email
+  before_create :create_activation_digest
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,
@@ -41,5 +42,16 @@ class User < ApplicationRecord
 
   def forget
     update_attribute(:remember_digest, nil)
+  end
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
