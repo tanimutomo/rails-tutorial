0ec18e24cd89d6af31137c33d99ad9d5bbdfe12b
tanimutomo <tanimutomo@gmail.com>
2020-08-29 16:46:23 +0900

    add: test for micropost post

--- sample_app/app/controllers/microposts_controller.rb
+++ sample_app/app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@
 
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
-  before_action :correct_User, only: :destroy
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
